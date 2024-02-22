37127b5fed03776a25e3e3112708fb3da5acb899
Mekonen-Abera <makoninabera@gmail.com>
2024-02-22 02:50:09 -0800

    HTML

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,36 +1,26 @@
 #!/usr/bin/python3
-"""Starts a Flask web application.
+"""script that starts a Flask web application"""
 
-The application listens on 0.0.0.0, port 5000.
-Routes:
-    /: Displays 'Hello HBNB!'.
-    /hbnb: Displays 'HBNB'.
-    /c/<text>: Displays 'C' followed by the value of <text>.
-"""
 from flask import Flask
 
 app = Flask(__name__)
 
 
-@app.route("/", strict_slashes=False)
-def hello_hbnb():
-    """Displays 'Hello HBNB!'."""
+@app.route('/', strict_slashes=False)
+def hello():
     return "Hello HBNB!"
 
 
-@app.route("/hbnb", strict_slashes=False)
+@app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """Displays 'HBNB'."""
     return "HBNB"
 
 
-@app.route("/c/<text>", strict_slashes=False)
-def c(text):
-    """Displays 'C' followed by the value of <text>."""
-    text = text.replace("_", " ")
-    return "C {}".format(text)
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    txt = text.replace('_', ' ')
+    return "C {}".format(txt)
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
-
+    app.run(host='0.0.0.0', debug=True)
--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
-"""Starts a Flask web application.
+"""script that starts a Flask web application"""
 
-The application listens on 0.0.0.0, port 5000.
-Routes:
-    /cities_by_states: HTML page with a list of all states and related cities.
-"""
 from models import storage
 from flask import Flask
 from flask import render_template
@@ -13,21 +9,15 @@ app = Flask(__name__)
 
 
 @app.route("/cities_by_states", strict_slashes=False)
-def cities_by_states():
-    """Displays an HTML page with a list of all states and related cities.
-
-    States/cities are sorted by name.
-    """
+def states_list():
     states = storage.all("State")
     return render_template("8-cities_by_states.html", states=states)
 
 
 @app.teardown_appcontext
-def teardown(exc):
-    """Remove the current SQLAlchemy session."""
+def teardown(exec):
     storage.close()
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
-
+    app.run(host="0.0.0.0", debug=True)
