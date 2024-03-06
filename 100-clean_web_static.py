2975d735fa98009109e015adba82e266955907af
Mekonen-Abera <makoninabera@gmail.com>
2024-03-06 00:51:54 -0800

    AirBnB clone - Deploy static

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -8,7 +8,7 @@ fab -f 100-clean_web_static.py do_clean:number=2
 import os
 from fabric.api import *
 
-env.hosts = ['52.87.155.66', '54.89.109.87']
+env.hosts = ['18.234.107.135', '34.234.66.160']
 
 
 def do_clean(number=0):
